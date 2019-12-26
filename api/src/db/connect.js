d2b35dfb99dc4919cd14584bf2253d6cf9fba9d6
Anselmo Battisti <anselmobattisti@gmail.com>
2019-12-26 12:22:32 -0300

    update

--- api/src/db/connect.js
+++ api/src/db/connect.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 mongoose.set('useUnifiedTopology', true);
 
-console.log("aaaa")
-console.log(process.env.MONGODB_URL)
-console.log("aaaa")
+//console.log("aaaa")
+//console.log(process.env.MONGODB_URL)
+//console.log("aaaa")
 
 /* istanbul ignore next */ 
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true}).then(() => {

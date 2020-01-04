ee355f5d83432417af85a12b8e9df3b6db545284
Anselmo Battisti <anselmobattisti@gmail.com>
2020-01-04 15:51:16 -0300

    funcoiando

--- frame_processor/process.py
+++ frame_processor/process.py
@@ -22,6 +22,9 @@ import os
 
 r  = int(sys.argv[2])
 
+# numero de pocos que foram inseridos na linha de comando, por padrao tem que ser 15
+qtd_pocos = ((len(sys.argv) - 4) / 3)+1
+
 img_name = sys.argv[1]
 
 path = img_name.split('/')
@@ -37,32 +40,77 @@ for rd, dd, fd in os.walk(path):
         if quadrante in file:
             files.append(os.path.join(rd, file))
 
+# armazena a posicao no primeiro frame de cada poco
+posicao_cada_poco = []
+
+################
+img_name = files[0]
+img_original = cv2.imread(img_name, 0)
+
+aux_nome = img_name.split('.')
+aux_nome_data = img_name.split('/')
+# nome_final = aux_nome_data[-1].split('_')
+# mills = nome_final[1].split('.')
+
+# pasta onde sera salva a imagem     
+folder_name = '/'.join(aux_nome_data[:-1])+'/'+quadrante
+
+# cria a pasta caso nao exista
+try:
+    os.makedirs(folder_name)
+except OSError:
+    pass
+
+# aqui estamos pegando a posicao de cada poco dentro da imagem que sera cortada 
+# de cada frame
+for i in range(1,qtd_pocos):
+    pos_nome = 3*(int(i)-1)+4
+    nome = str(sys.argv[pos_nome])
+
+    pos_y = 3*(int(i)-1)+5
+    y = int(sys.argv[pos_y])
+
+    pos_x = 3*(int(i)-1)+6
+    x = int(sys.argv[pos_x])
+
+    # crop image as a square
+    ax = x - r
+    bx = x + r
+    ay = y - r
+    by = y + r
+
+    img = img_original[ax:bx, ay:by]    
+    min_r = int(0.9*r)
+    max_r = int(1.2*r)
+
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 0.9, r, param1=1, param2=1, minRadius=min_r, maxRadius=max_r)
+    
+    # essa jah eh a imagem cortada por isso usamos o raio para todas as posicoes
+    kx = r
+    ky = r
+    kr = r
+
+    # se nao achou o circulo vai com fundo mesmo
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        kx, ky, kr = circles[0][0]
+
+    posicao_cada_poco.append({'kx':kx,'ky':ky, 'kr':kr})
+
 # processa todas as imagens do quadrante (por conta da posicao do x e y de cada poco e para distribuir o processamento)
 for img_name in files:
    
     img_original = cv2.imread(img_name, 0)
-
-    width = img_original.shape[0]
-    height = img_original.shape[1]
-
-    # numero de pocos que foram inseridos na linha de comando, por padrao tem que ser 15
-    qtd_pocos = ((len(sys.argv) - 4) / 3)+1
 
     aux_nome = img_name.split('.')
     aux_nome_data = img_name.split('/')
     nome_final = aux_nome_data[-1].split('_')
     mills = nome_final[1].split('.')
 
-    # cria a pasta caso nao exista
-    try:
-        os.makedirs(aux_nome[0])
-    except OSError:
-        pass
-
+    # lista com os pocos identificados dentro daquele frame
     pocos = []
 
-    for i in range(1,qtd_pocos):
-
+    for i in range(1,qtd_pocos):        
         pos_nome = 3*(int(i)-1)+4
         nome = str(sys.argv[pos_nome])
 
@@ -81,37 +129,18 @@ for img_name in files:
 
         # print(ax, ay, bx, by)
 
-        #     x  
-        #   y  . . . . . . . . . . . . . .
-        #      . . (ax,ay) - - - - . . . .
-        #      . . | . . . . . . . . . . .
-        #      . . | . . . . . . . . . . .
-        #      . . | . . . . . . . . . . .
-        #      . . | . . . . . . . (bx,by)
         # img eh a imagem cortada contendo o circulo selecionado pelo usuario
         img = img_original[ax:bx, ay:by]
         
+        # DEIXAR ISSO AQUI 
         # salva a imagem na pasta antes de pasar pelo circle
-        nome_img_poco_sem = aux_nome[0]+'/'+nome+'_org.'+aux_nome[1]
-        cv2.imwrite(nome_img_poco_sem,img)
+        # nome_img_poco_sem = folder_name+'/'+nome+'_'+mills[0]+'_org.'+aux_nome[1]
+        # cv2.imwrite(nome_img_poco_sem,img)
 
-        # ////////////
-        # img = cv2.medianBlur(img,5)
-        min_r = int(0.8*r)
-        max_r = int(1.2*r)
-
-        circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 0.9, r, param1=30, param2=50, minRadius=min_r, maxRadius=max_r)
-        
-        kx = r
-        ky = r
-        kr = r
-
-        # se nao achou o circulo vai com fundo mesmo
-        if circles is not None:
-            circles = np.uint16(np.around(circles))
-            kx, ky, kr = circles[0][0]
-    
-        # print(kx, kx, ax, ay, bx, by)
+        # essa jah eh a imagem cortada por isso usamos o raio para todas as posicoes
+        kx = posicao_cada_poco[i-1]['kx']
+        ky = posicao_cada_poco[i-1]['ky']
+        kr = posicao_cada_poco[i-1]['kr']
 
         # create a mask
         mask = np.full((img.shape[0], img.shape[1]), 0, dtype=np.uint8) 
@@ -126,7 +155,7 @@ for img_name in files:
         bk = cv2.bitwise_or(background, background, mask=mask)
         img_final = cv2.bitwise_or(fg, bk)
 
-        nome_img_poco = aux_nome[0]+'/'+nome+'.'+aux_nome[1]
+        nome_img_poco = folder_name+'/'+nome+'_'+mills[0]+'.'+aux_nome[1]
 
         # print(nome_img_poco)
         # pocos.append(Poco(nome, nome_img_poco))

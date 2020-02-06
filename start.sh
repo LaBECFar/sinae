7dc3fbdf335614bb95f6f1c740dff1389100d6e2
Anselmo Battisti <anselmobattisti@gmail.com>
2020-02-06 10:04:28 -0300

    sh

--- start.sh
+++ start.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-cecho(){
-    RED="\033[0;31m"
-    GREEN="\033[0;32m"
-    YELLOW="\033[1;33m"
-    # ... ADD MORE COLORS
-    NC="\033[0m" # No Color
-
-    printf "${!1}${2} ${NC}\n"
-}
-
-# Build Web Client
-cd web-client
-sudo npm run build
-
-cd ..
-
-sudo docker-compose build
-
 sudo docker-compose up

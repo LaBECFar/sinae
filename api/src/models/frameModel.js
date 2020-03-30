47c65a9f6ed7dfbec9a6f0297b3cce0f72fb74a0
Lucas K <lucas@webgenium.com.br>
2020-03-30 11:30:25 -0300

    Update frameModel.js

--- api/src/models/frameModel.js
+++ api/src/models/frameModel.js
@@ -1,64 +1,68 @@
-const mongoose = require('mongoose')
-const fs = require('fs')
-const Schema   = mongoose.Schema
+const mongoose = require("mongoose");
+const fs = require("fs");
+const Schema = mongoose.Schema;
 
-const framesSchema = new Schema({
+const framesSchema = new Schema(
+	{
+		analiseId: {
+			type: String,
+			required: true
+		},
 
-    'analiseId' : {
-        type: String,
-        required: true
-    },
+		// tempo em milisegundos
+		tempoMilis: {
+			type: Number,
+			required: true
+		},
 
-    // tempo em milisegundos
-    'tempoMilis': {
-        type: Number,
-        required: true
-    },
+		url: {
+			type: String,
+			required: true
+		},
 
-    'url' : {
-		type: String,
-		required: true
-    },
-    
-    'quadrante' : {
-        type: Number,
-		required: true
-    },
+		// numero do quadrante de 1 a 4
+		quadrante: {
+			type: Number,
+			required: true
+		},
 
-    'processado' : {
-        type: Boolean,
-        required: false,
-        default: false
-    },
+		processado: {
+			type: Boolean,
+			required: false,
+			default: false
+		},
 
-	'pocos' : [{
-        'nome' : {
-            type: String,
-            required: true
-        },
-    
-        'url' : {
-            type: String,
-            required: true
-        },
-   }],
+		pocos: [
+			{
+				nome: {
+					type: String,
+					required: true
+				},
 
-},{
-    timestamps: true
-});
+				url: {
+					type: String,
+					required: true
+				}
+			}
+		]
+	},
+	{
+		timestamps: true
+	}
+);
 
-framesSchema.methods.removerArquivos = function() { 
-    // remover imagens de poços
-    if(this.pocos && this.pocos.length > 0){
-        this.pocos.forEach((poco) => {
-            fs.unlinkSync(poco.url)
-        })
-    }
+framesSchema.methods.removerArquivos = function() {
+	// remover imagens de poços
+	if (this.pocos && this.pocos.length > 0) {
+		this.pocos.forEach(poco => {
+			fs.unlinkSync(poco.url);
+		});
+	}
 
-    // remover imagem do frame
-    if(this.url) {
-        fs.unlinkSync(this.url)
-    }
-}
+	// remover imagem do frame
+	if (this.url) {
+		fs.unlinkSync(this.url);
+	}
+};
 
-module.exports = mongoose.model('frame', framesSchema);
+module.exports = mongoose.model("frame", framesSchema);

4ac474117161a8c70e188e4e4bcbc9510afd18b8
Lucas K <lucas@webgenium.com.br>
2020-09-09 12:51:35 -0300

    frame_processor: using A.I. to generate images
    Adicionado no processamento dos frames um modelo criado pelo pessoal do LaBECFar para predição da posição dos parasitas nos poços.
    
    O modelo é usado para gerar uma imagem melhor para cada poço, otimizadas para o uso no cellprofiler.
    
    OBS: É necessário incluir o arquivo trained_model2.pkl no diretório /frame_processor pois o arquivo é maior que 100mb e não pode ser incluido no git
    Co-Authored-By: labecfar2 <60747208+labecfar2@users.noreply.github.com>

--- frame_processor/process.py
+++ frame_processor/process.py
@@ -4,6 +4,130 @@ import sys
 from pymongo import MongoClient
 from bson import ObjectId
 import os
+from torchvision.utils import save_image
+from fastai.vision import load_learner, open_image
+
+
+#funções necessárias para usar o modelo AI
+
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    p = len(gt_sorted)
+    gts = gt_sorted.sum()
+    intersection = gts - gt_sorted.float().cumsum(0)
+    union = gts + (1 - gt_sorted).float().cumsum(0)
+    jaccard = 1. - intersection / union
+    if p > 1: # cover 1-pixel case
+        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+    return jaccard
+
+def flatten_binary_scores2(scores, labels, ignore=None):
+    """
+    Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = scores.contiguous().view(-1)
+    labels = labels.contiguous().view(-1)
+    if ignore is None:
+        return scores, labels
+    valid = (labels != ignore)
+    vscores = scores[valid]
+    vlabels = labels[valid]
+    return vscores, vlabels
+
+def lovasz_hinge(logits, labels, per_image=False, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    print(logits.min(), logits.max(), logits.size())
+    logits=logits[:,1,:,:]
+    labels = labels.squeeze(1)
+    if per_image:
+        loss = mean(lovasz_hinge_flat(*flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), ignore))
+                          for log, lab in zip(logits, labels))
+    else:
+        loss = lovasz_hinge_flat(*flatten_binary_scores2(logits, labels, ignore))
+    return loss
+
+def lovasz_hinge_flat(logits, labels):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+
+    if len(labels) == 0:
+        # only void pixels, the gradients should be 0
+        return logits.sum() * 0.
+    signs = 2. * labels.float() - 1.
+    errors = (1. - logits * Variable(signs))
+    errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
+    perm = perm.data
+    gt_sorted = labels[perm]
+    grad = lovasz_grad(gt_sorted)
+    loss = torch.dot(torch.nn.functional.relu(errors_sorted), Variable(grad))
+    return loss
+
+def combined_loss(logits, labels):
+    logits=logits[:,1,:,:].float()
+    labels = labels.squeeze(1).float()
+    
+    lh_loss = lovasz_hinge_flat(*flatten_binary_scores2(logits, labels, ignore=0))
+    bce_loss = F.binary_cross_entropy_with_logits(logits, labels)
+    
+    return 0.8 * bce_loss + lh_loss
+
+def combined_loss2(logits, labels):
+    logits=logits[:,1,:,:].float() - logits[:,0,:,:].float()
+    labels = labels.squeeze(1).float()
+    
+    lh_loss = lovasz_hinge_flat(*flatten_binary_scores2(logits, labels))
+    bce_loss = F.binary_cross_entropy_with_logits(logits, labels)
+    
+    return 0.8 * bce_loss + lh_loss
+
+def dice_loss(logits, true, eps=1e-7):
+    """Computes the Sørensen–Dice loss.
+    Note that PyTorch optimizers minimize a loss. In this
+    case, we would like to maximize the dice loss so we
+    return the negated dice loss.
+    Args:
+        true: a tensor of shape [B, 1, H, W].
+        logits: a tensor of shape [B, C, H, W]. Corresponds to
+            the raw output or logits of the model.
+        eps: added to the denominator for numerical stability.
+    Returns:
+        dice_loss: the Sørensen–Dice loss.
+    """
+    num_classes = logits.shape[1]
+    if num_classes == 1:
+        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        true_1_hot_f = true_1_hot[:, 0:1, :, :]
+        true_1_hot_s = true_1_hot[:, 1:2, :, :]
+        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
+        pos_prob = torch.sigmoid(logits)
+        neg_prob = 1 - pos_prob
+        probas = torch.cat([pos_prob, neg_prob], dim=1)
+    else:
+        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+        probas = F.softmax(logits, dim=1)
+    true_1_hot = true_1_hot.type(logits.type())
+    dims = (0,) + tuple(range(2, true.ndimension()))
+    intersection = torch.sum(probas * true_1_hot, dims)
+    cardinality = torch.sum(probas + true_1_hot, dims)
+    dice_loss = (2. * intersection / (cardinality + eps)).mean()
+    return (1 - dice_loss)
+
 
 # run 
 # sudo python process.py 1 40 5e11f5add6f73000247d162e 1 55 60 2 136 60 3 220 60 4 55 142 5 136 142 6 220 142 7 55 225 8 136 225 9 220 225 10 55 310 11 136 310 12 220 310 13 50 395 14 136 395 15 215 395
@@ -69,6 +193,9 @@ aux_nome_data = img_name.split('/')
 # pasta onde serao salva a imagem, quero colocar o Q antes do quadrante
 folder_name = '/'.join(aux_nome_data[:-1])+'/Q'+quadrante
 
+# carrega o modelo AI
+learn = load_learner("./", "trained_model2.pkl")
+
 # cria a pasta para o quadrantecaso nao exista
 try:
     os.makedirs(folder_name)
@@ -121,7 +248,6 @@ for frame in frames:
     aux_nome_data = img_name.split('/')
     nome_final = aux_nome_data[-1].split('_')
     mills = nome_final[1].split('.')
-    # print(img_name)
 
     # lista com os pocos identificados dentro daquele frame
     pocos = []
@@ -139,11 +265,8 @@ for frame in frames:
         # crop image as a square
         ax = x - r
         bx = x + r
-
         ay = y - r
         by = y + r
-
-        # print(ax, ay, bx, by)
 
         # img eh a imagem cortada contendo o circulo selecionado pelo usuario
         img = img_original[ax:bx, ay:by]
@@ -173,8 +296,6 @@ for frame in frames:
 
         nome_img_poco = folder_name+'/'+nome+'_'+mills[0]+'.'+aux_nome[1]
 
-        # print(nome_img_poco)
-        # pocos.append(Poco(nome, nome_img_poco))
         poco = {}
         poco['nome'] = nome
         poco['url'] = nome_img_poco
@@ -182,6 +303,14 @@ for frame in frames:
         
         # salva a imagem na pasta
         cv2.imwrite(nome_img_poco,img_final)
+
+        
+        # passar poço pelo modelo de AI e gerar imagem
+        img_pred = open_image(nome_img_poco)
+        prediction = learn.predict(img_pred)
+        pred_float = prediction[1].float()
+        img_name = nome_img_poco.replace(".","_predicted.")
+        save_image(pred_float, img_name)
 
     # MONGO DB SALVA DADOS
     collection = db['frames']
